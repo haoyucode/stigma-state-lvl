f25d22d27ef1171f8da1ae8d984d59d579982c6c
Michael Kranz <mbkranz@gmail.com>
2024-03-27 15:19:08 -0500

    update package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='stigma-state-lvl',
+    name='stigma_state_lvl',
     packages=find_packages(),
     version='0.1.0',
     description='State level analyses of opioid use disorder (OUD) using the JCOIN MAARC Stigma Survey Protocol 2 ("in-depth")',
